f342f4cab14edd3f2da9cbc41c67fdcbd0a4d208
정민선 <minn602@gmail.com>
2021-02-28 20:58:49 +0900

    modify: hide api key and add gitignore file

--- app.js
+++ app.js
@@ -1,69 +1,79 @@
-//api key : a6bb1a24083582fa550d1cbc7c6e00ea
 //1. 웹 페이지가 로드되면 현재 위치 값 구하기
 //2. 구한 위치 값으로 api 연결
 //3. api에서 위치정보, 온도, 날씨코멘트, 아이콘 가져오기
 //4. 아이콘 연결
 
-'use strict';
+"use strict";
+
+const apiKey = API_KEY.apiKey;
 
 let currentIcon;
 
 function iconId(icon) {
-  if (icon === '01d') {
+  if (icon === "01d") {
     currentIcon = `CLEAR_DAY`;
-  } else if (icon === '01n') {
+  } else if (icon === "01n") {
     currentIcon = `CLEAR_NIGHT`;
-  } else if (icon === '02d') {
+  } else if (icon === "02d") {
     currentIcon = `PARTLY_CLOUDY_DAY`;
-  } else if (icon === '02n') {
+  } else if (icon === "02n") {
     currentIcon = `PARTLY_CLOUDY_NIGHT`;
-  } else if (icon === '03d' || icon === '03n' || icon === '04d' || icon === '04n') {
+  } else if (
+    icon === "03d" ||
+    icon === "03n" ||
+    icon === "04d" ||
+    icon === "04n"
+  ) {
     currentIcon = `CLOUDY`;
-  } else if (icon === '09d' || icon === '09n' || icon === '10d' || icon === '10n') {
+  } else if (
+    icon === "09d" ||
+    icon === "09n" ||
+    icon === "10d" ||
+    icon === "10n"
+  ) {
     currentIcon = `RAIN`;
-  } else if (icon === '11d' || icon === '11n') {
+  } else if (icon === "11d" || icon === "11n") {
     currentIcon = `WIND`;
-  } else if (icon === '13d' || icon === '13n') {
-    currentIcon = 'SNOW';
-  } else if (icon === '50d' || icon === '50n') {
-    currentIcon = 'FOG';
+  } else if (icon === "13d" || icon === "13n") {
+    currentIcon = "SNOW";
+  } else if (icon === "50d" || icon === "50n") {
+    currentIcon = "FOG";
   }
 }
 
 function setIcon(icon, id) {
-  const skycons = new Skycons({"color": "white"});
+  const skycons = new Skycons({ color: "white" });
   iconId(icon);
   skycons.play();
   return skycons.set(id, Skycons[currentIcon]);
 }
 
 function paintApp(city, country, temp, des, icon) {
-  const currentLocation = document.querySelector('.location'),
-        currentTemp = document.querySelector('.temperature'),
-        weatherDes = document.querySelector('.weather-des');
+  const currentLocation = document.querySelector(".location"),
+    currentTemp = document.querySelector(".temperature"),
+    weatherDes = document.querySelector(".weather-des");
   currentLocation.innerText = `${city} / ${country}`;
-  let temperature = (temp-273.15).toFixed(0);
+  let temperature = (temp - 273.15).toFixed(0);
   currentTemp.innerText = `${temperature} °C`;
   weatherDes.innerText = des;
-  setIcon(icon, document.querySelector('.icon'));
+  setIcon(icon, document.querySelector(".icon"));
 }
 
 function linkToApi(lon, lat) {
-  const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a6bb1a24083582fa550d1cbc7c6e00ea`;
+  const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
   fetch(api)
-    .then(response => {
+    .then((response) => {
       return response.json();
     })
-    .then(data => {
-      console.log(data);
+    .then((data) => {
       const city = data.name,
-            country = data.sys['country'],
-            temp = data.main.temp,
-            description = data.weather[0]['description'],
-            icon = data.weather[0]['icon'];
+        country = data.sys["country"],
+        temp = data.main.temp,
+        description = data.weather[0]["description"],
+        icon = data.weather[0]["icon"];
       paintApp(city, country, temp, description, icon);
-    })
+    });
 }
 
 function getLocation() {
@@ -74,13 +84,13 @@ function getLocation() {
     navigator.geolocation.getCurrentPosition((position) => {
       lon = position.coords.longitude.toFixed(2);
       lat = position.coords.latitude.toFixed(2);
-      linkToApi(lon, lat)
-    })
+      linkToApi(lon, lat);
+    });
   }
 }
 
 function loadEvent() {
-  window.addEventListener('load', getLocation);
+  window.addEventListener("load", getLocation);
 }
 
 function init() {
